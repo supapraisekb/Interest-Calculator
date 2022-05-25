e5cdbd03e1b77187bc47a7ff36c722e5cc4ca83e
Praise Eze <praisenimize@gmail.com>
2022-05-25 12:46:49 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 function compute()
 {
-		//Create variables from input
+	//Accept Variables from input by user
     let principal = document.getElementById("principal").value;
     let rate = document.getElementById("rate").value;
     let years = document.getElementById("years").value;
     
-    //do math
+    //Simple interest calculation
     let interest = principal * years * rate / 100;
     let endYear = new Date().getFullYear() + parseInt(years);
 
-    //compound interest formula
+    //compound interest calculation
     let compounded = principal * ((1 + ((rate/100)/12)) ** (12*years))
 		
-    //validate the principal. If negative or 0 alert user and focus on principal
+    //Check if the principal is valid. If negative or 0 inform the user and focus the cursor on principal 
     if(principal <= 0){
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return false;
     }
     
-    //display result
+    //display the result to the user
     document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br>at an interest rate of 
-    <mark>${rate}</mark>.<br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,<br>in the year <mark>${endYear}</mark><br>
+    <mark>${rate}</mark>.
+<br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,
+<br>in the year <mark>${endYear}</mark><br>
     <br>With monthly compounded interest your balance would instead be <mark>${compounded.toFixed(2)}</mark>`;
 }
 
-//display rate % by slider
+// rate % display using the slider
 function rangeReader()
 {
     let sliderValue = document.getElementById("rate").value;
